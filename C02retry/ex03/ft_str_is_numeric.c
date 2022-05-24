1d52c60cbde0d420293e0ab3e58085591453a6fd
Gabriela Pereira <55415365+GabrielaPRocha@users.noreply.github.com>
2022-05-24 00:22:48 -0300

    C02

---
+++ C02retry/ex03/ft_str_is_numeric.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrpere <gabrpere@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 17:02:28 by gabrpere          #+#    #+#             */
+/*   Updated: 2022/02/13 20:53:53 by gabrpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_numeric(char *str)
+{
+	while (*str != '\0')
+	{
+		if ((*str < '0' || *str > '9'))
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_str_is_alpha(char *str);
+
+int	main(void)
+{
+	char *str1;
+	char *str2;
+	char *str3;
+	char *str4;
+
+	str1 = calloc(10, sizeof(char));
+	str2 = calloc(10, sizeof(char));
+	str3 = calloc(10, sizeof(char));
+	str4 = calloc(10, sizeof(char));
+	str1 = "13212";
+	str2 = "abCde2gHi";
+	str3 = "2bCde2gHi";
+	str4 = "abCde2gH2";
+	printf("str1: %s, is_aplha: %d\n", str1, ft_str_is_alpha(str1));
+	printf("str2: %s, is_aplha: %d\n", str2, ft_str_is_alpha(str2));
+	printf("str3: %s, is_aplha: %d\n", str3, ft_str_is_alpha(str3));
+	printf("str4: %s, is_aplha: %d\n", str4, ft_str_is_alpha(str4));
+	return (0);
+}
